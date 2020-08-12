019caf20238dd9cc01d8f9605deb6a070a55ac90
chenshuai <chenshuai@amberweather.com>
2020-08-12 12:06:06 +0800

    change

--- ws_server.php
+++ ws_server.php
@@ -19,44 +19,45 @@ class WebSocketTest
 
         $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
             echo "Message: {$frame->data}\n";
-//            $msg = explode("#", $frame->data);
-//            $action = $msg[0];
-//            if ($action == "init") {
-//                $id = $msg[1];
-//                $key = $msg[2];
-//                $this->insertUserInfo($id, $key, $frame->fd);
-//
-//                // 返回给在线列表
-//                $allOnline = $this->getAllOnline();
-//                foreach ($this->server->connections as $fd) {
-//                    // 需要先判断是否是正确的websocket连接，否则有可能会push失败
-//                    if ($this->server->isEstablished($fd)) {
-//                        $server->push($fd, "list#$allOnline");
-//                    }
-//                }
-//            } else if ($action == "connect") {
-//                $bId = $msg[1];
-//                $key = $this->query($bId);
-//                if (!empty($key)) {
-//                    // 返回B的公钥
-//                    $server->push($frame->fd, "connect#$key");
-//                }
-//            } else if ($action == "chart") {
-//                $bId = $msg[1];
-//                $aesKey = $msg[2];
-//                $emsg = $msg[3];
-//                $fd = $this->queryFD($bId);
-//                if (!empty($fd) && $this->server->isEstablished($fd)) {
-//                    // 将信息转发给B
-//                    $server->push($fd, "chart#$aesKey#$emsg");
-//                }
-//            }
+            $msg = explode("#", $frame->data);
+            $action = $msg[0];
+            if ($action == "init") {
+                $id = $msg[1];
+                $key = $msg[2];
+                $this->insertUserInfo($id, $key, $frame->fd);
+
+                // 返回给在线列表
+                $allOnline = $this->getAllOnline();
+                $allOnlineFD = $this->getAllOnlineFD();
+                foreach ($allOnlineFD as $fd) {
+                    // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+                    if ($this->server->isEstablished($fd)) {
+                        $server->push($fd, "list#$allOnline");
+                    }
+                }
+            } else if ($action == "connect") {
+                $bId = $msg[1];
+                $key = $this->query($bId);
+                if (!empty($key)) {
+                    // 返回B的公钥
+                    $server->push($frame->fd, "connect#$key");
+                }
+            } else if ($action == "chart") {
+                $bId = $msg[1];
+                $aesKey = $msg[2];
+                $emsg = $msg[3];
+                $fd = $this->queryFD($bId);
+                if (!empty($fd) && $this->server->isEstablished($fd)) {
+                    // 将信息转发给B
+                    $server->push($fd, "chart#$aesKey#$emsg");
+                }
+            }
         });
 
         $this->server->on('close', function ($ser, $fd) {
             echo "client {$fd} closed\n";
-//            $this->offline($fd);
-            echo "当前连接数:" . sizeof($this->server->connections) . "\n";
+            $this->offline($fd);
+            echo "当前连接数:" . sizeof($this->server->connections) - 1 . "\n";
         });
 
         $this->server->start();
@@ -132,6 +133,19 @@ class WebSocketTest
             $ids .= ($row["id"] . ",");
         }
         return $ids;
+    }
+
+    protected function getAllOnlineFD()
+    {
+        $fds = array();
+        $result = mysqli_query($this->conn, "SELECT `fd` FROM `user_key` WHERE `online`=1;");
+        if (!$result) {
+            return $fds;
+        }
+        while ($row = mysqli_fetch_assoc($result)) {
+            array_push($fds, $row["fd"]);
+        }
+        return $fds;
     }
 }
 
