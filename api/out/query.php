51c6082627642abb456394ce6800001a0459e555
chenshuai <chenshuai@amberweather.com>
2020-08-13 19:48:57 +0800

    修改路径

--- api/out/query.php
+++ api/out/query.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 header('Content-type:application/json;charset=utf-8');
 
-require_once '/root/phpsocket/api/dispatch/Dispatcher.php';
+require_once __DIR__ . '/../../phpsocket/api/dispatch/Dispatcher.php';
 
 //$stime = microtime(true);
 $dispatcher = new Dispatcher(Dispatcher::TYPE_QUERY_GROUP);
